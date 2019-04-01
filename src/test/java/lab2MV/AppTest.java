999f47c9afd01224749d35f2e424498f3207e6bc
Alex Popsor <alexpopsor@MacBook-Air-Bogdan.local>
2019-04-01 16:18:18 +0300

    JUnit Tests

--- src/test/java/lab2MV/AppTest.java
+++ src/test/java/lab2MV/AppTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import Domain.Student;
 import Repository.StudentRepo;
 import Validator.StudentValidator;
+import Validator.ValidationException;
 import org.junit.Test;
 
 /**
@@ -22,6 +23,64 @@ public class AppTest
     }
 
     @Test
+    public void addStudentGoodId(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+        //int nrStudBefore = repo.size();
+
+        String id = "21";
+        String name = "Alex";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+
+        //int nrStudAfter = repo.size();
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentBadId(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+        int nrStud = repo.size();
+
+        String id = "-1";
+        String name = "Alex";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentNullId(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+        int nrStud = repo.size();
+
+        String id = null;
+        String name = "Alex";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentEmptyStringId(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+        int nrStud = repo.size();
+
+        String id = "";
+        String name = "Alex";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test
     public void addStudentGoodEmail() {
         StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
 
@@ -35,30 +94,197 @@ public class AppTest
 
     }
 
-    @Test
+    @Test(expected = ValidationException.class)
     public void addStudentBadEmail() {
         StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
 
         String id = "2206";
         String name = "Alex";
         int group = 931;
-        String email = "pbie2206@scs.ro";
+        String email = "pbie2206scsro";
         String teacher = "Profesor X";
 
         repo.save(new Student(id,name,group,email,teacher));
+    }
 
+    @Test(expected = NullPointerException.class)
+    public void addStudendNullEmail(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2206";
+        String name = "Alex";
+        int group = 931;
+        String email = null;
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudendEmptyStringEmail(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2206";
+        String name = "Alex";
+        int group = 931;
+        String email = "";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
     }
 
     @Test
-    public void addStudentBadId(){
+    public void addStudentGoodName(){
         StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
-        int nrStud = repo.size();
 
-        String id = "-1";
-        String name = "Alex";
+        String id = "2207";
+        String name = "Popsor Alexandru";
         int group = 931;
-        String email = "pbie2206scs";
+        String email = "pbie2206@scs.ubbcluj.ro";
         String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentBadName(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2207";
+        String name = "2Popsor Alexandru";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addStudentNullName(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2207";
+        String name = null;
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentEmptyStringName(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2207";
+        String name = "";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test
+    public void addStudentGoodProfessor(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2208";
+        String name = "Popsor Alexandru";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentBadProfessor(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2207";
+        String name = "Popsor Alexandru";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "2Profesor X";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addStudentNullProfessor(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2207";
+        String name = "Popsor Alexandru";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = null;
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentEmptyStringProfessor(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2207";
+        String name = "Popsor Alexandru";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test
+    public void addStudentGoodGroup(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2209";
+        String name = "Popsor Alexandru";
+        int group = 931;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Boca Madalina";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentBadGroup(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2209";
+        String name = "Popsor Alexandru";
+        int group = 100;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Boca Madalina";
+
+        repo.save(new Student(id,name,group,email,teacher));
+    }
+
+   @Test(expected = ValidationException.class)
+   public void addStudentBadSecondDigitGroup(){
+       StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+       String id = "2209";
+       String name = "Popsor Alexandru";
+       int group = 991;
+       String email = "pbie2206@scs.ubbcluj.ro";
+       String teacher = "Boca Madalina";
+
+       repo.save(new Student(id,name,group,email,teacher));
+   }
+
+    @Test(expected = ValidationException.class)
+    public void addStudentBadLastDigitGroup(){
+        StudentRepo repo =new StudentRepo(new StudentValidator(),"studenti.xml");
+
+        String id = "2209";
+        String name = "Popsor Alexandru";
+        int group = 930;
+        String email = "pbie2206@scs.ubbcluj.ro";
+        String teacher = "Boca Madalina";
 
         repo.save(new Student(id,name,group,email,teacher));
     }
